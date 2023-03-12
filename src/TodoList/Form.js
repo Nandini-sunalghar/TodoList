cae2ed06a7546ede771f2f0eb4fd9ebe7f384a5e
Nandini <snandiniuday@gmail.com>
2023-03-12 21:14:15 +0530

    todolist app changes

--- src/TodoList/Form.js
+++ src/TodoList/Form.js
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 
-function Form() {
+function Form(props) {
   const [input, setInput] = useState("");
+
+  const handleChange = (e) =>{
+    setInput(e.target.value);
+  }
 
   const handleSubmit = (e) => {
     console.log(e);
+
+    props.onSubmit({
+      id: Math.floor(Math.random()* 10000),
+      text: input,
+    });
     e.preventDefault();
+    setInput('');
   };
+  
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -14,9 +25,14 @@ function Form() {
           type="text"
           placeholder="Enter Task Todo..."
           value={input}
+          onChange={handleChange}
         ></input>
-        <button>Add</button>
+        <button onClick={props.AddTodo}>Add</button>
       </form>
+      <div>
+        <ul>
+        </ul>
+      </div>
     </div>
   );
 }
